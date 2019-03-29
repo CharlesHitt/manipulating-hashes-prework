70df42c9141f8f0d4f80a238226b633d9006d63c
Charles Hitt <charles.a.hitt@gmail.com>
2019-03-29 01:20:53 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -17,7 +17,8 @@ def first_challenge
 contacts.each do |person, data|
   data.each do |key, value|
     if key == :favorite_icecream_flavors
-      value.delete 
+      value.delete_if do |flavor|
+        
 
 
   #remember to return your newly altered contacts hash!
